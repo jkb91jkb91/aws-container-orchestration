1d241e2e220a730a926b3cff91e14824967fd954
Kuba <jakub.g2610199@gmail.com>
2026-03-08 10:22:34 +0100

    added eks on fargate

---
+++ eks/eks-on-fargate/terraform/modules/eks/output.tf
@@ -0,0 +1,9 @@
+output "eks_cluster_name" { value = aws_eks_cluster.this.name }
+output "eks_cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
+output "eks_cluster_ca_data" { value = aws_eks_cluster.this.certificate_authority[0].data }
+
+
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
